5452f10da20f4227f71a10a77689ae1b0fa9caf0
PATTARASAI <PATTARASAI@set.or.th>
2018-07-22 22:17:17 +0700

    - Add working hours label

--- settrade-cmu/ViewController.swift
+++ settrade-cmu/ViewController.swift
@@ -10,22 +10,28 @@ import UIKit
 import MapKit
 
 class ViewController: UIViewController {
-
+    let STT_LOCATION_LATITUDE = 13.763877;
+    let STT_LOCATION_LONGTITUDE = 100.56770770000003;
+    
     @IBOutlet weak var mapWithSttAddress: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let sttLocation: CLLocation = CLLocation(latitude: 13.763877, longitude: 100.56770770000003)
-        let sttLocationCor2D = CLLocationCoordinate2D(latitude: 13.763877, longitude: 100.56770770000003)
+        initializeSttLocationOnMap()
+    }
+    
+    func initializeSttLocationOnMap() {
+        let sttLocation: CLLocation = CLLocation(latitude: STT_LOCATION_LATITUDE, longitude: STT_LOCATION_LONGTITUDE)
+        let sttLocationCor2D = CLLocationCoordinate2D(latitude: STT_LOCATION_LATITUDE, longitude: STT_LOCATION_LONGTITUDE)
         let coordinateRegion: MKCoordinateRegion = MKCoordinateRegionMakeWithDistance(sttLocation.coordinate,
-                                                                  1000, 1000);
+                                                                                      1000, 1000);
         mapWithSttAddress.setRegion(coordinateRegion, animated: true)
         
         let sttAnnotation = MKPointAnnotation();
         sttAnnotation.title = "Settrade, co. ltd";
         sttAnnotation.coordinate = sttLocationCor2D;
-
-        mapWithSttAddress.addAnnotation(sttAnnotation);
         
+        mapWithSttAddress.addAnnotation(sttAnnotation);
     }
 
     override func didReceiveMemoryWarning() {
