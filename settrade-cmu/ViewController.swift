21b53be6e1b66be1dc2d1d8d2432b8358c7b1dbe
PATTARASAI <PATTARASAI@set.or.th>
2018-07-22 22:12:15 +0700

    - Add map

--- settrade-cmu/ViewController.swift
+++ settrade-cmu/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapWithSttAddress: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let sttLocation: CLLocation = CLLocation(latitude: 13.763877, longitude: 100.56770770000003)
+        let sttLocationCor2D = CLLocationCoordinate2D(latitude: 13.763877, longitude: 100.56770770000003)
+        let coordinateRegion: MKCoordinateRegion = MKCoordinateRegionMakeWithDistance(sttLocation.coordinate,
+                                                                  1000, 1000);
+        mapWithSttAddress.setRegion(coordinateRegion, animated: true)
+        
+        let sttAnnotation = MKPointAnnotation();
+        sttAnnotation.title = "Settrade, co. ltd";
+        sttAnnotation.coordinate = sttLocationCor2D;
+
+        mapWithSttAddress.addAnnotation(sttAnnotation);
+        
     }
 
     override func didReceiveMemoryWarning() {
